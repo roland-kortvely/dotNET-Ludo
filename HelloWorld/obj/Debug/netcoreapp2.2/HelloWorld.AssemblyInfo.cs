867d61663ebb7c52cbd8aaca7f7e0ee1e4564b5a
Roland Körtvely <mikronum@gmail.com>
2019-02-13 21:37:46 +0100

    Cleanup

--- HelloWorld/obj/Debug/netcoreapp2.2/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/netcoreapp2.2/HelloWorld.AssemblyInfo.cs
@@ -7,10 +7,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ludo")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ludo")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ludo")]
+[assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
