7385c8c7142ba716e60a3b9d33a46a1d63fee561
Roland Körtvely <mikronum@gmail.com>
2019-03-18 20:37:56 +0100

    LudoWeb preload

---
+++ LudoWeb/Controllers/ScoreController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ludo.Entities;
+using Ludo.Interfaces;
+using Ludo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LudoWeb.Controllers
+{
+    [Route("api/score")]
+    public class ScoreController : Controller
+    {
+        private readonly IScoreService _service = new ScoreService();
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Score> Get()
+        {
+            return (IEnumerable<Score>) _service.GetTop();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LudoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LudoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
